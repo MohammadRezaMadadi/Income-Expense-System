ae7751221ff8bfc930ec7f63d9a4b79a2c9a348e
Mohammad Reza Madadi <madadimr@yahoo.com>
2020-05-13 20:07:42 +0100

    added Income in module

--- in_ex/models.py
+++ in_ex/models.py
@@ -3,11 +3,22 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+
 class Expense(models.Model):
     text = models.CharField(max_length=300)
     date = models.DateTimeField(auto_now=False, auto_now_add=False)
     amount = models.BigIntegerField()
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='in_ex')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.date} - {self.text}" 
+
+
+class Income(models.Model):
+    text = models.CharField(max_length=300)
+    date = models.DateTimeField(auto_now=False, auto_now_add=False)
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.date} - {self.text}" 
