558ae61a660210ca125ea8c6bef658bd07f65492
Mohammad Reza Madadi <madadimr@yahoo.com>
2020-05-13 17:26:38 +0100

    this is first Expense

--- in_ex/models.py
+++ in_ex/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Expense(models.Model):
+    text = models.CharField(max_length=300)
+    date = models.DateTimeField(auto_now=False, auto_now_add=False)
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='in_ex')
+
+    def __str__(self):
+        return f"{self.date} - {self.text}" 
